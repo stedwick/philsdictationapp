8178e05a7d7b102b7c5f36273d921dc66aeed8fc
Philip Brocoum <philip.brocoum@gmail.com>
2023-11-22 09:50:06 -0800

    UNTESTED react-speech-recognition

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,58 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 // import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Buttons } from "./components/Buttons";
 import { Toaster } from "react-hot-toast";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import useNavigatorOnline from "use-navigator-online";
 
 function App() {
   const [dictationState, setDictationState] = useState<"on" | "off" | "paused">(
     "off"
   );
   const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  const { isOnline, isOffline, backOnline, backOffline } = useNavigatorOnline();
+
+  const {
+    transcript,
+    interimTranscript,
+    finalTranscript,
+    resetTranscript,
+    listening,
+    browserSupportsSpeechRecognition,
+    // browserSupportsContinuousListening,
+    isMicrophoneAvailable,
+  } = useSpeechRecognition();
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+  if (!isMicrophoneAvailable) {
+    // Render some fallback conten
+    return <span>Couldn't access microphone.</span>;
+  }
+  // if (browserSupportsContinuousListening) {
+  //   // SpeechRecognition.startListening({ continuous: true });
+  // } else {
+  //   // Fallback behaviour
+  // }
+
+  useEffect(() => {
+    if (dictationState == "on" && !listening) {
+      SpeechRecognition.startListening();
+    } else if (dictationState == "off" && listening) {
+      SpeechRecognition.stopListening();
+    }
+  }, [dictationState, listening]);
+
+  useEffect(() => {
+    if (dictationState == "on") {
+      textareaRef.current!.value += transcript;
+    }
+    resetTranscript();
+  }, [transcript]);
 
   return (
     <>
@@ -20,6 +64,11 @@ function App() {
         <h1 className="text-xl text-center">
           Welcome to Phil's Dictation App!
         </h1>
+
+        <p>
+          {isOnline ? "online" : "not online"}{" "}
+          {isOffline ? "offline" : "not offline"}
+        </p>
 
         <textarea
           placeholder="Click 🎙️ Start Dictating button below..."
