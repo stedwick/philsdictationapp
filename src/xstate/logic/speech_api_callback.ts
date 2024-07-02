99dfda131930a51437d169f0cdbf36a68736796f
Philip Brocoum <philip.brocoum@gmail.com>
2024-07-02 07:49:20 -0700

    speechApiLogic

---
+++ src/xstate/logic/speech_api_callback.ts
@@ -0,0 +1,14 @@
+import { fromCallback, EventObject } from "xstate";
+
+export default fromCallback<EventObject, { recognition: SpeechRecognition }>(
+  ({ sendBack, input: { recognition } }) => {
+    console.log("speechApiLogic spawned"); // DEBUG
+    recognition.onresult = (event) => {
+      // https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognitionResultList
+      for (let i = 0; i < event.results.length; i++) {
+        console.log(event.results[i][0].transcript); // DEBUG
+        sendBack({ type: "hear", result: event.results[i] });
+      }
+    };
+  }
+);
