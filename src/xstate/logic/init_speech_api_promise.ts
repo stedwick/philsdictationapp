1ec69eb93fcb5fa1169fcaa7c7babaa2ee2ac685
Philip Brocoum <philip.brocoum@gmail.com>
2024-07-02 07:49:57 -0700

    init speech api

---
+++ src/xstate/logic/init_speech_api_promise.ts
@@ -0,0 +1,39 @@
+import { fromPromise } from "xstate";
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API/Using_the_Web_Speech_API
+export default fromPromise(async function () {
+  const SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+  const SpeechGrammarList =
+    window.SpeechGrammarList || window.webkitSpeechGrammarList;
+  // const SpeechRecognitionEvent =
+  // window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
+
+  const colors = [
+    "Syncta", // TODO
+    "aqua",
+    "azure",
+    "beige",
+    "bisque",
+    "black",
+    "blue",
+    "brown",
+    "chocolate",
+    "coral" /* … */,
+  ];
+  const grammar = `#JSGF V1.0; grammar colors; public <color> = ${colors.join(
+    " | "
+  )};`;
+
+  const recognition = new SpeechRecognition();
+  const speechRecognitionList = new SpeechGrammarList();
+  speechRecognitionList.addFromString(grammar, 1);
+  recognition.grammars = speechRecognitionList;
+
+  recognition.continuous = false;
+  recognition.lang = "en-US";
+  recognition.interimResults = false;
+  recognition.maxAlternatives = 1;
+
+  return recognition;
+});
