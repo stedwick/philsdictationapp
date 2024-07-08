02920dff48b4fb2cd567343c95824d621d081397
Philip Brocoum <philip.brocoum@gmail.com>
2024-07-08 14:48:33 -0700

    Here already logs, minus one debug statement

--- src/xstate/logic/speech_api_callback.ts
+++ src/xstate/logic/speech_api_callback.ts
@@ -1,11 +1,10 @@
-import { fromCallback, EventObject } from "xstate";
+import { EventObject, fromCallback } from "xstate";
 
 export default fromCallback<EventObject, { recognition: SpeechRecognition }>(
   ({ sendBack, input: { recognition } }) => {
     recognition.onresult = (event) => {
       // https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognitionResultList
       for (let i = 0; i < event.results.length; i++) {
-        console.log(event.results[i][0].transcript); // DEBUG
         sendBack({ type: "hear", result: event.results[i] });
       }
     };
