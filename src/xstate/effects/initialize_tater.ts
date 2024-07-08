69618c3d17535f76a1e5903ee0649fd704cc3edb
Philip Brocoum <philip.brocoum@gmail.com>
2024-07-08 14:45:12 -0700

    window.taterRef = taterRef;

--- src/xstate/effects/initialize_tater.ts
+++ src/xstate/effects/initialize_tater.ts
@@ -2,5 +2,5 @@ import { AnyActorRef } from "xstate";
 
 export default function initializeTater(taterRef: AnyActorRef) {
   taterRef.send({ type: "initialize" });
-  window.taterRef = taterRef; // DEBUG
+  window.taterRef = taterRef;
 }
