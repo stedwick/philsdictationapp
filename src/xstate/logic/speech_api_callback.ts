557416f60a2df5420cb6c20ffb04c2f0678eeb86
Philip Brocoum <philip.brocoum@gmail.com>
2024-07-08 16:25:37 -0700

    const i = event.results.length - 1;

--- src/xstate/logic/speech_api_callback.ts
+++ src/xstate/logic/speech_api_callback.ts
@@ -4,9 +4,14 @@ export default fromCallback<EventObject, { recognition: SpeechRecognition }>(
   ({ sendBack, input: { recognition } }) => {
     recognition.onresult = (event) => {
       // https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognitionResultList
-      for (let i = 0; i < event.results.length; i++) {
-        sendBack({ type: "hear", result: event.results[i] });
-      }
+
+      // I think the results just keep piling up. Always send the last one.
+      const i = event.results.length - 1;
+      sendBack({ type: "hear", result: event.results[i] });
+
+      // for (let i = 0; i < event.results.length; i++) {
+      //   sendBack({ type: "hear", result: event.results[i] });
+      // }
     };
     recognition.onend = () => {
       sendBack({ type: "turnOff" });
