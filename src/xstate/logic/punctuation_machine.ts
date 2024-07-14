165de49acade0873afe3caa667e52033334a4fd6
Philip Brocoum <philip.brocoum@gmail.com>
2024-07-13 17:03:58 -0700

    Punctuation getting there

--- src/xstate/logic/punctuation_machine.ts
+++ src/xstate/logic/punctuation_machine.ts
@@ -20,8 +20,11 @@ const punctuationRegex = new RegExp(
   '\\b(' + Object.keys(punctuationMap).join('|') + ')\\b',
   "gi"
 );
+window.regex = punctuationRegex;
 
+// Should I replace spaces around -?
 const charsWithNoSpaces = "-";
+
 const charsWithOnlySpaceBefore = "(";
 const charsWithOnlySpaceAfter = ",.:;?!)";
 const charsThatCapitalizeNext = ".?!\\\n";
@@ -30,12 +33,12 @@ const charsThatCapitalizeNext = ".?!\\\n";
 // FIXME: space OR beginning of line
 const charsWithOnlySpaceBeforeRegex = new RegExp(`([${charsWithOnlySpaceBefore}])\\s+`, "g");
 const charsWithOnlySpaceAfterRegex = new RegExp(`\\s+([${charsWithOnlySpaceAfter}])`, "g");
-const charsThatCapitalizeNextRegex = new RegExp(`([${charsThatCapitalizeNext}]\\s+)(\\w)`, "g");
+const charsThatCapitalizeNextRegex = new RegExp(`([${charsThatCapitalizeNext}]\\W+)(\\w)`, "g");
 
-const capitalizeNextRegex = new RegExp(`(^|[${charsThatCapitalizeNext}])\\s*$`);
-window.regex = capitalizeNextRegex;
+const capitalizeNextRegex = new RegExp(`(^|[${charsThatCapitalizeNext}])\\W*$`);
 const spaceComesNextRegex = new RegExp(`[^\\s${charsWithOnlySpaceBefore}${charsWithNoSpaces}]$`);
 const spaceComesBeforeRegex = new RegExp(`^[^${charsWithOnlySpaceAfter}]`);
+// window.regex = capitalizeNextRegex;
 
 type PunctuationMachineContext = {
   before: string;
@@ -59,73 +62,12 @@ export const punctuationMachine = setup({
   states: {
     punctuating: {
       type: "final",
+      // The order below is very important.
       entry: [
         // trim
         assign({
           text: ({ context: { text } }) => text.trim(),
         }),
-
-        // punctuationRegex
-        assign({
-          text: ({ context: { text } }) => {
-            return text.replace(punctuationRegex, function(matched: string) {
-              const matchedLowerCase = matched.toLowerCase();
-              const mappedValue = punctuationMap[matchedLowerCase];
-              return mappedValue || matched;
-            });
-          }
-        }),
-        ({ context: { text } }) => console.log("punc: [", text, "]"),
-
-        // Special rules
-        assign({
-          text: ({ context: { text } }) => {
-            // These have special spacing rules, and it matters if they are left or right, so we can't use the map above.
-            // Open and close quotes
-            text = text.replace(/open[ -]quotation\s+/gi, '"');
-            text = text.replace(/\s+close[ -]quotation/gi, '"');
-            // Apostrophe s
-            text = text.replace(/\s+apostrophe s/gi, "'s");
-            return text;
-          }
-        }),
-        ({ context: { text } }) => console.log("special's: [", text, "]"),
-
-        // Is padding a problem?
-        // Pad text with the last/first non-space characters from before and after.
-        // This affects punctuation, for example:
-        // "...end." + text + "(and..."
-        //
-        // assign(({ context: { before, text, after } }) => {
-        //   const padBefore = before.match(/[^\s]*\s*$/)?.[0] || "";
-        //   const padAfter = after.match(/^\s*[^\s]*/)?.[0] || "";
-        //   return {
-        //     text: padBefore + text + padAfter,
-        //     numCharsPaddedBefore: padBefore.length,
-        //     numCharsPaddedAfter: padAfter.length,
-        //   };
-        // }),
-        // ({ context: { text } }) => console.log("<pad>: [", text, "]"),
-
-        // Capitalize the first letter of each sentence
-        assign({
-          text: ({ context: { before, text } }) => {
-            if (before.match(capitalizeNextRegex)) {
-              return text.replace(/^(.)/, (char) => char.toUpperCase());
-            } else {
-              return text;
-            }
-          }
-        }),
-        ({ context: { text } }) => console.log("cap 1st: [", text, "]"),
-
-        // Capitalize within utterance (saying two sentences with a period in one breath)
-        assign({
-          text: ({ context: { text } }) => {
-            return text.replace(charsThatCapitalizeNextRegex, (_match, p1, p2) => (p1 + p2.toUpperCase()));
-          }
-        }),
-        ({ context: { text } }) => console.log("end. cap: [", text, "]"),
 
         // Add space before sentence
         assign({
@@ -150,6 +92,52 @@ export const punctuationMachine = setup({
           }
         }),
         ({ context: { text } }) => console.log(". space: [", text, "]"),
+
+        // punctuationRegex
+        assign({
+          text: ({ context: { text } }) => {
+            return text.replace(punctuationRegex, function(matched: string) {
+              const matchedLowerCase = matched.toLowerCase();
+              const mappedValue = punctuationMap[matchedLowerCase];
+              return mappedValue || matched;
+            });
+          }
+        }),
+        ({ context: { text } }) => console.log("punc: [", text, "]"),
+
+        // Special rules
+        assign({
+          text: ({ context: { text } }) => {
+            // These have special spacing rules, and it matters if they are left or right, so we can't use the map above.
+            // Open and close quotes
+            text = text.replace(/new[ -]quotation\s*/gi, '"');
+            text = text.replace(/\s*end[ -]quotation/gi, '"');
+            // Apostrophe s
+            text = text.replace(/\s*apostrophe(s| s)?/gi, "'s");
+            return text;
+          }
+        }),
+        ({ context: { text } }) => console.log("special's: [", text, "]"),
+
+        // Capitalize the first letter of each sentence
+        assign({
+          text: ({ context: { before, text } }) => {
+            if (before === '' || before.match(capitalizeNextRegex)) {
+              return text.replace(/^\W*(\w)/, (char) => char.toUpperCase());
+            } else {
+              return text;
+            }
+          }
+        }),
+        ({ context: { text } }) => console.log("cap 1st: [", text, "]"),
+
+        // Capitalize within utterance (saying two sentences with a period in one breath)
+        assign({
+          text: ({ context: { text } }) => {
+            return text.replace(charsThatCapitalizeNextRegex, (_match, p1, p2) => (p1 + p2.toUpperCase()));
+          }
+        }),
+        ({ context: { text } }) => console.log("end. cap: [", text, "]"),
 
         // charsWithOnlySpaceBefore
         assign({
@@ -183,3 +171,20 @@ export const punctuationMachine = setup({
     },
   },
 });
+
+// Is padding a problem?
+// Pad text with the last/first non-space characters from before and after.
+// This affects punctuation, for example:
+// "...end." + text + "(and..."
+//
+// assign(({ context: { before, text, after } }) => {
+//   const padBefore = before.match(/[^\s]*\s*$/)?.[0] || "";
+//   const padAfter = after.match(/^\s*[^\s]*/)?.[0] || "";
+//   return {
+//     text: padBefore + text + padAfter,
+//     numCharsPaddedBefore: padBefore.length,
+//     numCharsPaddedAfter: padAfter.length,
+//   };
+// }),
+// ({ context: { text } }) => console.log("<pad>: [", text, "]"),
+
