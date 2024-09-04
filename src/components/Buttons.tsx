948ac1d8378e79e89d2feb9fc3a0dffc7c41a58f
Philip Brocoum <philip.brocoum@gmail.com>
2024-09-03 21:29:58 -0700

    Solved the weird mobile glitches by not allowing dictation to start when the textarea element is focused.

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -6,17 +6,48 @@ import {
 import { YoutubeIcon } from "lucide-react";
 import { AnyMachineSnapshot } from "xstate";
 import { taterMachineContext } from "../xstate/tater_machine_context";
+import { useCallback, useEffect, useState } from "react";
+import { isMobile } from "../xstate/helpers/mobile";
 
 const micStateSelector = (state: AnyMachineSnapshot) => state.context.micState;
 const textareaValueSelector = (state: AnyMachineSnapshot) =>
   state.context.textareaCurrentValues.value;
+const textareaElSelector = (state: AnyMachineSnapshot) => state.context.textareaEl;
 
 export const Buttons = () => {
   const taterRef = taterMachineContext.useActorRef();
   const micState = taterMachineContext.useSelector(micStateSelector);
+  const textareaEl = taterMachineContext.useSelector(textareaElSelector) as HTMLTextAreaElement | null;
 
   const textareaValue = taterMachineContext.useSelector(textareaValueSelector);
   const cutEnabled = textareaValue ? "" : "btn-disabled";
+
+  // The following block prevents starting dictation while the textarea el is
+  // active. For some reason this causes problems on mobile.
+  const [textareaIsActive, setTextareaIsActive] = useState(false);
+  const handleTextareaFocus = useCallback(() => {
+    setTextareaIsActive(document.activeElement === textareaEl);
+  }, [textareaEl, setTextareaIsActive])
+
+  useEffect(function watchTextareaEl() {
+    if (isMobile && textareaEl) {
+      handleTextareaFocus();
+
+      ["focus", "blur"].forEach(
+        (event) => {
+          textareaEl.addEventListener(event, handleTextareaFocus);
+        }
+      );
+
+      return () => {
+        ["focus", "blur"].forEach(
+          (event) => {
+            textareaEl.removeEventListener(event, handleTextareaFocus);
+          }
+        );
+      }
+    }
+  }, [textareaEl, handleTextareaFocus]);
 
   return (
     <div className="flex flex-wrap justify-center lg:justify-between flex-col lg:flex-row gap-x-12 mb-2">
@@ -26,6 +57,7 @@ export const Buttons = () => {
             <button
               className="btn btn-outline btn-error"
               onClick={() => taterRef.send({ type: "turnOn" })}
+              disabled={textareaIsActive}
             >
               <MicrophoneIcon className="h-6 w-6"></MicrophoneIcon>Start
               Dictating
@@ -35,6 +67,7 @@ export const Buttons = () => {
             <button
               className="btn btn-error relative"
               onClick={() => taterRef.send({ type: "turnOff" })}
+              disabled={textareaIsActive}
             >
               <MicrophoneIcon className="h-6 w-6 animate-ping absolute left-4"></MicrophoneIcon>
               <MicrophoneIcon className="h-6 w-6"></MicrophoneIcon>
@@ -48,6 +81,7 @@ export const Buttons = () => {
             <button
               className="btn btn-outline"
               onClick={() => taterRef.send({ type: "wake" })}
+              disabled={textareaIsActive}
             >
               <BellSnoozeIcon className="h-6 w-6"></BellSnoozeIcon>Say "wake up"
             </button>
