594093a1847b9497921bce205643c6d381cb2d68
Philip Brocoum <philip.brocoum@gmail.com>
2024-12-22 11:34:26 -0500

    Remove unused imports and commented-out code in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import App from "./App";
 import "./styles.css";
 import { taterMachineContext } from "./xstate/tater_machine_context";
 
-import { createActor } from "xstate";
-import { speechmaticsLogic } from "./xstate/logic/speechmatics_callback";
-window.speechmaticsActor = createActor(speechmaticsLogic);
+// import { createActor } from "xstate";
+// import { speechmaticsLogic } from "./xstate/logic/speechmatics_callback";
+// window.speechmaticsActor = createActor(speechmaticsLogic);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
